5ae1332fd54a1ea21103f15ee2d4cd9ac8d3fad1
Michael Barlow <michaelbarlow7@gmail.com>
2019-02-18 12:11:35 +1100

    Completed UI for news articles

--- app/src/main/java/com/michaelbarlow/fairfaxapplication/NewsArticleListAdapter.kt
+++ app/src/main/java/com/michaelbarlow/fairfaxapplication/NewsArticleListAdapter.kt
@@ -2,16 +2,20 @@ package com.michaelbarlow.fairfaxapplication
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import kotlinx.android.synthetic.main.item_news_article.view.*
 
 class NewsArticleListAdapter : RecyclerView.Adapter<NewsArticleListAdapter.NewsArticleViewHolder>(){
 
-    class NewsArticleViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class NewsArticleViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val newsArticleHeadline = view.newsArticleHeadLine
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsArticleViewHolder {
-        val textView = LayoutInflater.from(parent.context).inflate(R.layout.item_news_article, parent, false) as TextView
-        return NewsArticleViewHolder(textView)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news_article, parent, false)
+        return NewsArticleViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -19,6 +23,6 @@ class NewsArticleListAdapter : RecyclerView.Adapter<NewsArticleListAdapter.NewsA
     }
 
     override fun onBindViewHolder(holder: NewsArticleViewHolder, position: Int) {
-        holder.textView.text = "Article: $position"
+//        holder.newsArticleHeadline.text = "Article: $position"
     }
 }
