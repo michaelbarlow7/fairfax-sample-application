903f7ee85881b95de4ece9ce6a652b90739eac30
Michael Barlow <michaelbarlow7@gmail.com>
2019-02-18 22:09:43 +1100

    Added unit test for viewmodel

--- app/src/main/java/com/michaelbarlow/fairfaxapplication/NewsArticle.kt
+++ app/src/main/java/com/michaelbarlow/fairfaxapplication/NewsArticle.kt
@@ -1,15 +1,15 @@
 package com.michaelbarlow.fairfaxapplication
 
 data class NewsArticle(val id: Long = -1,
-                       val headline: String?,
-                       val theAbstract: String?,
-                       val byLine: String?,
+                       val headline: String? = null,
+                       val theAbstract: String? = null,
+                       val byLine: String? = null,
                        val timeStamp: Long = -1,
-                       val url: String?,
-                       val relatedImages: List<RelatedImage>?) {
+                       val url: String? = null,
+                       val relatedImages: List<RelatedImage>? = null) {
 
     data class RelatedImage(val id: Long = -1,
-                            val url: String?,
+                            val url: String? = null,
                             val width: Int = 0,
                             val height: Int = 0)
 
---
+++ app/src/test/java/com/michaelbarlow/fairfaxapplication/NewsArticleViewModelUnitTest.kt
@@ -0,0 +1,79 @@
+package com.michaelbarlow.fairfaxapplication
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.mock
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import org.junit.Test
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.Mockito.verify
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(MockitoJUnitRunner::class)
+class NewsArticleViewModelUnitTest {
+
+    @Rule
+    @JvmField
+    var rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var newsArticleRepository: NewsArticleRepository
+
+    lateinit var newsArticleViewModel: NewsArticleViewModel
+
+    val mockSuccessArticleList by lazy {
+        val firstArticle = NewsArticle()
+        listOf(firstArticle)
+    }
+
+    @Before
+    fun setup() {
+        newsArticleViewModel = NewsArticleViewModel(newsArticleRepository)
+    }
+
+    @Test
+    fun successResponse_isCorrectFormat() {
+        val observer: Observer<Resource<List<NewsArticle>>> = mock()
+        Mockito.`when`(newsArticleRepository.getNewsArticles())
+            .thenReturn(Single.just(mockSuccessArticleList))
+        val newsArticles = newsArticleViewModel.getNewsArticles()
+        newsArticles.observeForever(observer)
+        verify(observer).onChanged(Resource(Resource.Status.SUCCESS, mockSuccessArticleList))
+    }
+
+    @Test
+    fun errorResponse_returnsErrorStatus() {
+        val observer: Observer<Resource<List<NewsArticle>>> = mock()
+        Mockito.`when`(newsArticleRepository.getNewsArticles())
+            .thenReturn(Single.error(Exception()))
+        val newsArticles = newsArticleViewModel.getNewsArticles()
+        newsArticles.observeForever(observer)
+        verify(observer).onChanged(Resource(Resource.Status.ERROR, null))
+    }
+
+    @Test
+    fun loadingTest_returnsLoadingStatus() {
+        val observer: Observer<Resource<List<NewsArticle>>> = mock()
+        Mockito.`when`(newsArticleRepository.getNewsArticles())
+            .thenReturn(Single.just(mockSuccessArticleList))
+        val newsArticles = newsArticleViewModel.getNewsArticles()
+        newsArticles.observeForever(observer)
+        newsArticleViewModel.refreshData()
+        verify(observer).onChanged(Resource(Resource.Status.LOADING, null))
+        verify(observer).onChanged(Resource(Resource.Status.SUCCESS, mockSuccessArticleList))
+
+    }
+}
