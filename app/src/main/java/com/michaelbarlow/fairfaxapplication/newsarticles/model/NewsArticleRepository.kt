d03dd6843dce547d4145d37e57b96876f67f3bcb
Michael Barlow <michaelbarlow7@gmail.com>
2019-02-19 10:00:32 +1100

    Sorting articles by latest first

--- app/src/main/java/com/michaelbarlow/fairfaxapplication/newsarticles/model/NewsArticleRepository.kt
+++ app/src/main/java/com/michaelbarlow/fairfaxapplication/newsarticles/model/NewsArticleRepository.kt
@@ -1,20 +1,19 @@
 package com.michaelbarlow.fairfaxapplication.newsarticles.model
 
-import com.michaelbarlow.fairfaxapplication.newsarticles.model.FairfaxAPIService
-import com.michaelbarlow.fairfaxapplication.newsarticles.model.NewsArticle
 import io.reactivex.Single
 
 /**
  * Basic class that handles retrieving data for news articles.
  *
  * Refines the data a little bit (i.e. returns list of NewsArticles rather than raw data)
+ * and sorts so that latest articles appear first
  */
 class NewsArticleRepository(private val fairfaxAPIService: FairfaxAPIService) {
 
     fun getNewsArticles(): Single<List<NewsArticle>> {
         return fairfaxAPIService.getNewsArticles()
             .map {
-                it.assets
+                it.assets.sortedByDescending { t -> t.timeStamp }
             }
     }
 }
